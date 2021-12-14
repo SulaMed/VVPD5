2eb48b824c3884fee4568b4af72f763501461f37
Bebrovich <sultikmed@gmail.com>
2021-12-14 20:37:49 +0600

    test3

--- op4/tests/test_deadline_score.py
+++ op4/tests/test_deadline_score.py
@@ -4,7 +4,9 @@ import pytest
 
 @pytest.mark.parametrize("pass_date, deadline_date, score", [('11-11-2011', '12-11-2011', 5),
                                                              ('13-11-2012', '12-11-2012', 4),
-                                                             ('11-12-2012', '11-11-2012', 3)])
+                                                             ('21-11-2012', '11-11-2012', 3),
+                                                             ('31-11-2012', '01-11-2012', 1),
+                                                             ('19-11-2012', '01-11-2012', 2)])
 def test_deadline_score(pass_date, deadline_date, score):
     assert deadline_score(pass_date, deadline_date) == score
 
